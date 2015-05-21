e02851b2da9b569eb8a2b866429ab4284b9634bb
Jazz <jazzcousins@gmail.com>
2015-05-22 00:09:27 +0100

    upload_stage_2

---
+++ Source/DragonGame/DragonGame.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DragonGame.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DragonGame, "DragonGame" );
